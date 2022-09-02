e5ffc201bb1cbeecade03ba4428e69569b32689b
Alexandre Ribeiro <alexandresilva58@gmail.com>
2022-09-02 20:21:35 -0300

    Criado Tela de Login visualmente completa

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -8,43 +8,94 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-     body: Column(
-
-       children: [
-         Expanded(
-           child: Container( color: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.green,
             ),
-         ),
-         Expanded(
-           child: Container(
-             padding: const EdgeInsets.symmetric(
-               horizontal: 32,
-               vertical: 40
-             ),
-             decoration: const BoxDecoration(
-               color: Colors.white,
-               borderRadius: BorderRadius.vertical(
-                 top: Radius.circular(45)
-               )
-             ),
-             child: Column(
-               children: [
-                 CustomFormField(
-                   icon: Icons.email,
-                   label: 'E-mail',
-                   type: TextInputType.emailAddress,
-                 ),
-                 CustomFormField(
-                   icon: Icons.lock,
-                   label: 'Senha',
-                   isSecret: true,
-                 ),
-               ],
-             ),
-           ),
-         ),
-       ],
-     ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const CustomFormField(
+                  icon: Icons.email,
+                  label: 'E-mail',
+                  type: TextInputType.emailAddress,
+                ),
+                const CustomFormField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
+                SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                        onPressed: () {},
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 18),
+                        ))),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.red, fontSize: 14),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('Ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18)),
+                        side: const BorderSide(width: 2, color: Colors.green)),
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
