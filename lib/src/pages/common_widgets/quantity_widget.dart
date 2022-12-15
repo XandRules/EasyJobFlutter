c6166a2ffc7b0a4e2f5bb5c68427b95f14e65a15
Alexandre Ribeiro <alexandresilva58@gmail.com>
2022-12-14 22:51:44 -0300

    Criado widget de add ao carrinho

--- lib/src/pages/common_widgets/quantity_widget.dart
+++ lib/src/pages/common_widgets/quantity_widget.dart
@@ -5,6 +5,79 @@ class QuantityWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 2,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          _Quantity_Button(
+            color: Colors.grey,
+            icon: Icons.remove,
+            onPressed: () {},
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 6),
+            child: Text(
+              '1 Hora',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          _Quantity_Button(
+            color: Colors.grey,
+            icon: Icons.remove,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Quantity_Button extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const _Quantity_Button({
+    Key? key,
+    required this.color,
+    required this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        borderRadius: BorderRadius.circular(50),
+        onTap: onPressed,
+        child: Ink(
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            icon,
+            color: Colors.white,
+            size: 16,
+          ),
+        ),
+      ),
+    );
   }
 }
